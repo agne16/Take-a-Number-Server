277a14c09b3c763f94f1739ecc58fea37ace08c0
T.J <agne16@up.edu>
2016-02-19 18:56:26 -0800

    added bash scripts for server

---
+++ 1_compile_project.bash
@@ -0,0 +1,1 @@
+javac -d bin -sourcepath src -cp lib/jdom-2.0.6.jar src/main/java/edu/up/projects/engineering/*.java
---
+++ 2_run_project.bash
@@ -0,0 +1,1 @@
+java -cp bin:lib/jdom-2.0.6.jar edu.up.projects.engineering.ServerMain
